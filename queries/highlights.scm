248ced3030bb257567cba6074088f25a5dada32d
AltCode <altcode@vivaldi.net>
2026-03-04 18:10:30 +0100

    Support HTML entities and missing keywords
    Adds an `entity` node to the grammar that should behaves exactly the
    same as the one defined by tree-sitter-html
    
    Also adds the `:key` special attribute, and all missing reserved do-end
    keywords (`catch`, `rescue`, `after`, `else`) so that they are parsed as
    `partial_expression_value`

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,53 +1,46 @@
-; HEEx delimiters
+; General HEEx delimiters
 [
-  "%>"
-  "--%>"
-  "-->"
-  "/>"
-  "<!"
-  "<!--"
   "<"
-  "<%!--"
-  "<%"
-  "<%#"
-  "<%%="
-  "<%="
+  "<!"
   "</"
-  "</:"
   "<:"
+  "</:"
   ">"
+  "/>"
   "{"
   "}"
 ] @punctuation.bracket
 
+; HEEx directive expressions can use the following tag delimiters:
+;     `<%` and `%>`
+;     `<%=` and `%>`
+;     `<%%` and `%>`
+;     `<%%=` and `%>`
+(directive) @keyword
+
+; HEEx comments can use the following tag delimiters:
+;     `<!--` and `-->`
+;     `<%!--` and `--%>`
+;     `<%#` and `%>`
+(comment) @comment
+
 ; HEEx operators are highlighted as such
 "=" @operator
+
+; HEEx entities are highligted as special strings
+(entity) @string.special
 
 ; HEEx inherits the DOCTYPE tag from HTML
 (doctype) @constant
 
-; HEEx comments are highlighted as such
-(comment) @comment
-
 ; Tree-sitter parser errors
 (ERROR) @error
 
-; HEEx tags and slots are highlighted as HTML
+; HEEx tags and slots are highlighted as HTML tags
 [
- (tag_name) 
- (slot_name) 
+  (tag_name)
+  (slot_name)
 ] @tag
-
-; HEEx attributes are highlighted as HTML attributes
-(attribute_name) @attribute
-
-; HEEx special attributes are highlighted as keywords
-(special_attribute_name) @keyword
-
-[
-  (attribute_value)
-  (quoted_attribute_value)
-] @string
 
 ; HEEx components are highlighted as Elixir modules and functions
 (component_name
@@ -56,3 +49,20 @@
     (function) @function
     "." @punctuation.delimiter
   ])
+
+; HEEx attributes are highlighted as HTML attributes
+(attribute_name) @attribute
+
+; HEEx special attributes can be any of the following:
+;     `:let`
+;     `:if`
+;     `:for`
+;     `:key`
+;     `:stream`
+(special_attribute_name) @keyword
+
+; HEEx attribute values are highlighted as HTML attribute values
+[
+  (attribute_value)
+  (quoted_attribute_value)
+] @string
